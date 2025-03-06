b205836be9a0684373467991e2193df454d44764
rosales <rosales@localhost>
2025-03-06 09:14:37 -0700

    :D

---
+++ src/mx/itson/carrito/entidades/Carro.java
@@ -0,0 +1,116 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mx.itson.carrito.entidades;
+
+/**
+ *
+ * @author rosales
+ */
+
+public class Carro {
+
+    /**
+     * @return the kilometers
+     */
+    public double getKilometers() {
+        return kilometers;
+    }
+
+    /**
+     * @param kilometers the kilometers to set
+     */
+    public void setKilometers(double kilometers) {
+        this.kilometers = kilometers;
+    }
+
+    /**
+     * @return the color
+     */
+    public String getColor() {
+        return color;
+    }
+
+    /**
+     * @param color the color to set
+     */
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    /**
+     * @return the year
+     */
+    public int getYear() {
+        return year;
+    }
+
+    /**
+     * @param year the year to set
+     */
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    /**
+     * @return the brand
+     */
+    public String getBrand() {
+        return brand;
+    }
+
+    /**
+     * @param brand the brand to set
+     */
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    /**
+     * @return the category
+     */
+    public String getCategory() {
+        return category;
+    }
+
+    /**
+     * @param category the category to set
+     */
+    public void setCategory(String category) {
+        this.category = category;
+    }
+    
+    
+    private String color;
+    
+    private int year;
+    
+    private String brand;
+    
+    private String category;
+    
+    private double kilometers;
+   
+    /**
+     * Calcula el tiempo que tarda el carro en recorrer una distancia.
+     * @param distance Distancia que recorrerá el carro.
+     * @param speed Velocidad en la que avanza el carro.
+     * @return Tiempo en el que llegará el carro.
+     */
+    public double arriveTime(double distance, double speed) {
+      
+        this.kilometers += distance;
+
+      //La línea de arriba es lo mismo que ésta de abajo
+      
+      //this.kilometers = this.kilometers + distance;
+      
+        /*double time = distance/speed;
+        return time;
+        */
+        return distance/speed;
+      }
+    
+ }
+
---
+++ src/mx/itson/carrito/ui/Main.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mx.itson.carrito.ui;
+
+import java.util.Scanner;
+import mx.itson.carrito.entidades.Carro;
+
+/**
+ *
+ * @author rosales
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Ingresa la distancia:");
+        double distance = sc.nextDouble();
+        
+        System.out.println("Ingresa la velocidad:");
+        double speed = sc.nextDouble();
+        
+        //Generamos una instancia de la clase carro
+        
+        Carro bmw = new Carro();
+        
+        bmw.setBrand("BMW");
+        bmw.setCategory("Sport");
+        bmw.setColor("Silver");
+        bmw.setYear(2025);
+        bmw.setKilometers(10000);
+       
+               
+       double time = bmw.arriveTime(distance, speed);
+       
+        System.out.println("El tiempo que le tomará al carro " + bmw.getBrand() 
+                + " de color " + bmw.getColor() 
+                + " de clase " + bmw.getCategory() 
+                +  " será " + time + " horas " 
+                + " y ahora su kilometraje es de " + bmw.getKilometers() );
+    }
+}
